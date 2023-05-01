2ea7747f9783ca8dc9c26d23ff557e9d9f529602
Aman Srivastava <aman4863@gmail.com>
2023-05-01 21:37:14 +0530

    added

---
+++ M1/lec001/02. print_pattern_1/Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World.");
+        System.out.println("I");
+        System.out.println("am");
+        System.out.println("here.");
+        
+    }
+}
