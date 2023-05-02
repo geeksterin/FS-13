1f397a8caa0ebac594aec105ea111dca20498ab5
Aman Srivastava <aman4863@gmail.com>
2023-05-02 21:14:23 +0530

    added

---
+++ M1/lec002/02. sum_and_diff/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        int x = scn.nextInt();
+        int y = scn.nextInt();
+        
+        System.out.println(x+y);
+        System.out.println(x-y);
+    }
+}
