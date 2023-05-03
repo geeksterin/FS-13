cb93287ee361771141c63a5f8d8f90be5a941ec0
Aman Srivastava <aman4863@gmail.com>
2023-05-03 20:49:49 +0530

    added

---
+++ M1/lec003/02. greater_than_100/Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        boolean ans = n > 100;
+        System.out.println( ans );
+        
+        
+    }
+}
