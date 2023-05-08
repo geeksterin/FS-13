7eca3a3dc2c02daaa9d573e99de154bd6d15f05c
Aman Srivastava <aman4863@gmail.com>
2023-05-08 20:54:39 +0530

    added

---
+++ M1/lec007/02. switch_calculator/Solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        int N = scn.nextInt();
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        
+        switch(N){
+            case 10:
+                System.out.println(a+b);
+                break;
+            case 20:
+                System.out.println(a-b);
+                break;
+            case 30:
+                System.out.println(a*b);
+                break;
+            case 40:
+                System.out.println(a%b);
+                break;
+            case 50:
+                System.out.println(a/b);
+                break;
+            default:
+                System.out.println("Enter a valid number");
+                
+        }
+        
+    }
+}
