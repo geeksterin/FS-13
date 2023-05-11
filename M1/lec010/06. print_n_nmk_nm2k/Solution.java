3d46dabaf52bc5b0e21566b7ddc6cd80322f975a
Aman Srivastava <aman4863@gmail.com>
2023-05-11 21:38:18 +0530

    added

---
+++ M1/lec010/06. print_n_nmk_nm2k/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        
+        // for(int i = n; i >= 0; i -= k ){
+        //     System.out.println(i);
+        // }
+         for(int i = 0; (n-i*k) >= 0; i++){
+            System.out.println(n-i*k);
+        }
+    }
+}
