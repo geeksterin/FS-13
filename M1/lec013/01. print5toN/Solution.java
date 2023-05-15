4d9002f45ae8010d29f217cee5b910d93c41b181
Aman Srivastava <aman4863@gmail.com>
2023-05-15 20:27:12 +0530

    added

---
+++ M1/lec013/01. print5toN/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int i = 5;
+        while(i <= n){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
